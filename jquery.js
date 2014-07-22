45767c562f52b8bc2cbe677dfe489b74af931f1b
jeffreybates <jeffreybates@Jeffreys-MacBook-Air.local>
2014-07-22 14:58:19 -0700

    initial upload

---
+++ jquery.js
@@ -0,0 +1,58 @@
+
+
+$( "a" ).css("text-decoration","none");
+$( "a" ).css("color","black");
+//$( "a" ).css("padding-top","5%");
+
+$( "li" ).css("list-style","none");
+$( "li" ).css("display","inline-block");
+$( "li" ).css("margin-left","20px");
+$( "li" ).css("margin-right","20px");
+
+$( "ul" ).css("text-align","center");
+
+$( "#banner" ).css("padding-top","10px" );
+$( "#banner" ).css("font-size","3em" );
+$( "#banner" ).css("max-font-size","4em" );
+$( "#banner" ).css("border-radius", "2px" );
+$( "#banner" ).css("text-align", "center" );
+$( "#banner" ).css("color", "#99FFFF" );
+ 
+
+
+$( "header" ).css("padding-left", "2%" );
+$( "header" ).css("padding-right", "2%" );
+
+$( ".mywords" ).css( "padding-left", "2%" );
+$( ".mywords" ).css( "padding-right", "2%" );
+
+$( ".mywords" ).children().css("display","inline-block");
+
+
+$( "#casing" ).css("padding-left", "2%" );
+$( "#casing" ).css("padding-right", "2%" );
+
+$( ".profilepic" ).css("max-width", "97%");
+$( ".profilepic" ).css( "border", "1px solid #99FFFF");
+$( ".profilepic" ).css( "border-radius", "5px");
+$( ".profilepic" ).css( "display", "inline-block");
+$( ".profilepic" ).css( "text-align", "center");
+
+
+if($(window).width() >= 640){
+$(" .profilepic ").css( "float" , "left" );
+$(" .profilepic ").css( "max-width" , "50%" );
+$(" .mywords ").css( "float" , "left" );
+$(" .mywords " ).css( "max-width", "50%")
+}
+
+$( "footer" ).css( "clear", "both" );
+$( "footer" ).css( "position", "relative" );
+$( "footer" ).css( "clear", "both" );
+
+
+$( "#social" ).children().children().children().children().css("border-radius", "20px");
+$( "#social" ).children().children().children().children().css("border", "1px solid #99FFFF" );
+
+
+// $( ".myclass.otherclass" ).css( "border", "13px solid red" );
